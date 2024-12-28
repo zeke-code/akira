7cb0c33354deed098f999c2f5f03b1e1d17866e9
zeke-code <116798879+DemaControlsUs@users.noreply.github.com>
2024-12-28 17:50:32 +0100

    chore: upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
