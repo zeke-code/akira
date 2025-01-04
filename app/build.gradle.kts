bd8b8d16a60e7e79d02548a0da4c63bcd58b5cf9
zeke-code <116798879+DemaControlsUs@users.noreply.github.com>
2025-01-04 19:34:29 +0100

    chore: test updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.zekecode.akira_financialtracker"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
